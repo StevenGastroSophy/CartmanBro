c9462dd92e7c2a0796a1263d660a458c854d3217
kaspersky518 <kaspersky518@gmail.com>
2017-01-15 18:05:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -129,7 +129,7 @@ def LANDpar():
     
     try:
 
-        url = 'https://ebank.landbank.com.tw/infor/infor.aspx?__eventtarget=querycurrencyl'
+        url = 'https://ebank.landbank.com.tw/infor/infor.aspx?__eventtarget=querycurrency'
         header = {"User-Agent": 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36'}
 
         res=requests.get(url)       
